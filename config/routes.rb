37517d6c82b35de462fe9a23dd0838adbcc1ac02
gengrui <gengr.r@gmail.com>
2016-03-11 01:49:44 +0000

    install Devise to environment and set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/show'
+
   resources :users
+  
+  root to: "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
