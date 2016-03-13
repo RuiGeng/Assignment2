1dcd9ec9488fcac34e97753dac9985261859a2b6
gengrui <gengr.r@gmail.com>
2016-03-13 03:08:53 +0000

    add vlidation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates_presence_of :name, on: [:create, :save, :update]
+  validates_length_of :name, maximum: 50
+  
+  validates_presence_of :email, on: [:create, :save, :update]
+  validates_format_of    :email,    :with  => Devise.email_regexp, :if => :email_changed?
+  validates_uniqueness_of :email, :case_sensitive => false, :if => :email_changed?
+
+  validates_presence_of :password, on: [:create, :save, :update]
+  validates_confirmation_of :password, on: [:create, :save, :update]
+  validates_length_of :password, :within => Devise.password_length
 end
