eb57416c6c504978b9555bb605e821a9097db04e
gengrui <gengr.r@gmail.com>
2016-03-15 23:10:27 +0000

    fix sign up problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  validates :name, presence: true, on: [:create, :save, :update],
-            length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   
-  validates :email, :presence => true, length: { in: 3..50 },
+  validates :email, presence: true, length: { in: 3..50 },
             uniqueness: { case_sensitive: false },
             format: { with: Devise.email_regexp }, on: [:create, :save, :update]
   
-  validates :password, :presence => true, :confirmation => true,
+  validates :password, presence: true, confirmation: true,
+            length: { in: Devise.password_length }, on: [:create, :save, :update]
+  
+  validates :password_confirmation, :presence => true,
             length: { in: Devise.password_length }, on: [:create, :save, :update]
 
 end
