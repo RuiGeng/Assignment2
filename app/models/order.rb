3ae12730095803f283c0dc511a1a56c36c4e4800
gengrui <gengr.r@gmail.com>
2016-03-15 01:58:16 +0000

    update vlidation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,19 +1,34 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   
-  phone_number = /\d{3}-\d{3}-\d{4}/
-  email_address = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
-  canadian_postal_code = /\A[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1}[ -]?\\d{1}[A-Z]{1}\\d{1}\z/
+  before_save { self.email = email.downcase }
+  
+  VALID_PHONE_REGEX = /\d{3}-\d{3}-\d{4}/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_POSTALCODE_REGEX = /\A[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1}[ -]?\\d{1}[A-Z]{1}\\d{1}\z/
   
   validates :user_id, presence: true
+  
   validates :orderDate, presence: true
+  
   validates :firstName, presence: true, length: { maximum: 25 }
+  
   validates :lastName, presence: true, length: { maximum: 25 }
-  validates :phoneNumber, presence: true, format: { with: phone_number, message: "Please Enter a Valid Phone Number" }
-  validates :email, presence: true, format: { with: email_address, message: "Please Enter a Valid Eemail Address" }
+  
+  validates :phoneNumber, presence: true, 
+            format: { with: VALID_PHONE_REGEX, message: "Please Enter a Valid Phone Number" }
+            
+  validates :email, presence: true, length: { maximum: 254 }, 
+            format: { with: VALID_EMAIL_REGEX , message: "Please Enter a Valid Eemail Address" }
+            
   validates :addressLine1, presence: true, length: { maximum: 50 }
+  
   validates :addressLine2, presence: true, length: { maximum: 50 }
+  
   validates :province, presence: true, length: { maximum: 25 }
+  
   validates :city, presence: true, length: { maximum: 25 }
-  validates :postalCode, presence: true, format: { with: canadian_postal_code, message: "Please Enter a Valid Postal Code" }
+  
+  validates :postalCode, presence: true, 
+            format: { with: VALID_POSTALCODE_REGEX, message: "Please Enter a Valid Postal Code" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :orders
+
+  before_save { self.email = email.downcase }
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-  validates_presence_of :name, on: [:create, :save, :update]
-  validates_length_of :name, maximum: 50
   
-  validates_presence_of :email, on: [:create, :save, :update]
-  validates_format_of    :email,    :with  => Devise.email_regexp, :if => :email_changed?
-  validates_uniqueness_of :email, :case_sensitive => false, :if => :email_changed?
-
-  validates_presence_of :password, on: [:create, :save, :update]
-  validates_confirmation_of :password, on: [:create, :save, :update]
-  validates_length_of :password, :within => Devise.password_length
+  validates :name, presence: true, on: [:create, :save, :update],
+            length: { maximum: 50 }
+  
+  validates :email, :presence => true, 
+            :length => {:minimum => 3, :maximum => 254},
+            uniqueness: { case_sensitive: false }, :email => true, 
+            :with  => Devise.email_regexp, on: [:create, :save, :update]
+            
+            
+  validates :password, :presence => true, :confirmation => true,
+            :within => Devise.password_length, on: [:create, :save, :update],
+            :unless => :force_submit
+            
 end
