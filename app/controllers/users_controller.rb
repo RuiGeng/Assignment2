0c471d0cf7137bdf38fbfdf4089a40fba553aafd
gengrui <gengr.r@gmail.com>
2016-03-16 15:12:54 +0000

    try controller function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  #before_filter :authenticate_user!
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  #before_filter :authenticate_user!, excpet: [:index]
+  #before_action :set_user, only: [:show, :edit, :update, :destroy]
+  #before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -73,4 +74,12 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    # # Confirms a logged-in user.
+    # def logged_in_user
+    #   unless logged_in?
+    #     flash[:danger] = "Please log in."
+    #     redirect_to user_session_path
+    #   end
+    # end
 end
