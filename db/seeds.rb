20cd5ad48200b81bc693d895b89042033ce9565a
gengrui <gengr.r@gmail.com>
2016-03-17 02:13:42 +0000

    add a User into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(name:  "Rui",
+             email: "gengr.r@gmail.com",
+             password:              "19791126",
+             password_confirmation: "19791126",
+             admin: true)
